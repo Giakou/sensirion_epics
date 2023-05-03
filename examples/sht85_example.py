28218f89bd079ef33fd7a25c8e7a43603ebb8c70
Georgios Giakoustidis <georgios.giakoustidis@desy.de>
2023-05-03 21:47:29 +0900

    ENH: Update SHT85 example code to use context manager

--- examples/sht85_example.py
+++ examples/sht85_example.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     # Check S/N
     logger.info(f'serial number = {mysensor.sn}')
 
-    try:
+    with mysensor.i2c_daq():
         while True:
             # Single shot mode is preferred due to less current consumption (x8-x200) in idle state
             mysensor.single_shot()
@@ -26,8 +26,3 @@ if __name__ == '__main__':
             logger.info(f'Relative Humidity = {mysensor.rh}%')
             logger.info(f'Dew Point = {mysensor.dp} °C')
             time.sleep(mysensor.mps)
-
-    except (KeyboardInterrupt, SystemExit):
-        logger.warning("Killing Thread...")
-    finally:
-        mysensor.stop()
