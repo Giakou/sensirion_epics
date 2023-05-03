667d650ab221409869dd723a27e4f8aaa05224b2
Georgios Giakoustidis <georgios.giakoustidis@desy.de>
2023-05-03 21:46:19 +0900

    ENH: SHT class inherits from SMBus class and use of context manager

--- sensirion_i2c/sht/sht.py
+++ sensirion_i2c/sht/sht.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import time
-import smbus2
 import functools
+from contextlib import contextmanager
+from smbus2 import SMBus
 
 import sensirion_i2c.sht.utils.conversion_utils as conversion_utils
 import sensirion_i2c.utils.log_utils as log_utils
@@ -11,16 +12,31 @@ import sensirion_i2c.utils.log_utils as log_utils
 logger = log_utils.get_logger()
 
 
-class SHT:
+class SHT(SMBus):
 
-    def __init__(self, bus):
-        # Assertion checks
-        assert bus not in [0, 2], f'Bus number "{bus}" is not allowed, because they are reserved! Choose another one! '
-
+    def __init__(self):
+        super().__init__()
         # Define properties
-        self._bus = smbus2.SMBus(bus)
         self._addr = None
+        self._bus = None
         self.check_crc_bool = True
+
+    @contextmanager
+    def i2c_daq(self):
+        try:
+            self.open_rpi_bus()
+            yield
+        except (KeyboardInterrupt, SystemExit):
+            logger.warning("Killing Thread...")
+        finally:
+            self.stop()
+            self.close()
+
+    def open_rpi_bus(self):
+        # Assertion check
+        assert self._bus not in [0, 2], f'Bus number "{self._bus}" is not allowed, because they are reserved! ' \
+                                        f'Choose another one!'
+        self.open(self._bus)
 
     def calculate_crc(kw):
         """Decorator function to calculate crc"""
@@ -75,32 +91,36 @@ class SHT:
     @calculate_crc(kw='sn')
     def _sn(self, cmd):
         """Output of the serial number"""
-        self.write_i2c_block_data_sht(cmd)
-        self.data = self.read_i2c_block_data_sht(6)
+        self.write_i2c_data_sht(cmd)
+        self.data = self.read_i2c_data_sht(6)
         return (self.data[0] << 24) + (self.data[1] << 16) + (self.data[3] << 8) + self.data[4]
 
-    def read_i2c_block_data_sht(self, length=32):
-        return self.bus.read_i2c_block_data(self.addr, 0x00, length)
+    def read_i2c_data_sht(self, length=32):
+        return self.read_i2c_block_data(self.addr, 0x00, length)
 
-    def write_i2c_block_data_sht(self, cmd):
+    def write_i2c_data_sht(self, cmd):
         """Wrapper function for writing block data to SHT85 sensor"""
-        self.bus.write_i2c_block_data(self.addr, register=cmd[0], data=cmd[1:])
+        if len(cmd) == 1:
+            cmd = cmd[0] if isinstance(cmd, list) else cmd
+            self.write_byte(self.addr, cmd)
+        else:
+            self.write_i2c_block_data(self.addr, register=cmd[0], data=cmd[1:])
         time.sleep(conversion_utils.WT[self.rep])
 
     def general_call_reset(self):
         """General Call mode to rese all devices on the same I2C bus line (not device specific!). This command only
         works if the device is able to process I2C commands."""
         logger.warning('Applying General Call Reset... This is not device specific!')
-        self.bus.write_byte(0x00, 0x06)
+        self.write_byte(0x00, 0x06)
 
     def interface_reset(self, addr):
         logger.info('Interface reset...')
         # Toggling SDA
         for i in range(9):
-            self.bus.write_byte(addr, 0xFF)
+            self.write_byte(addr, 0xFF)
             time.sleep(0.01)
 
         # Send the Start sequence before the next command
-        self.bus.write_byte(addr, 0x35)
-        self.bus.write_byte(addr, 0x17)
+        self.write_byte(addr, 0x35)
+        self.write_byte(addr, 0x17)
 
